6d28f6340c2368d096bc9effafb87607e815df2b
Preethu T J <preethuj>
2016-06-10 17:56:26 +0530

    &&&&&

--- project.local/public_html/index.php
+++ project.local/public_html/index.php
@@ -8,31 +8,6 @@
 
 company and organizations to analyze information for important decision
 
-making. Opinion mining is also called as sentiment analysis which involves in building a
-
-system to gather and examine opinions about the product made in reviews or tweets,
-
-comments, blog posts on the web. Sentiment is classified automatically for important
-
-applications such as opinion mining and summarization. To make valuable decisions in
-
-marketing analysis where implement sentiment classification efficiently. Reviews contain
-arch is in phrase-
-
-level opinion mining. It performs finer-grained analysis and directly looks at the opinion in
-
-online reviews. The proposed system is based on phrase-level to examine customer
-
-reviews. Phrase-level opinion mining is also well-known as aspect based opinion mining. It
-
-is used to extract most important aspects of an item and to predict the orientation of each
-
-aspect from the item reviews. The projected system implements aspect extraction using
-
-frequent itemset mining in customer product reviews and mining opinions whether it is
-
-positive or negative opinion. It identifies sentiment orientation of each aspect by supervised
-
-learning algorithms in customer reviews.</p>
+making. </p>
 </body>
 </html>
