b97dd0b92c8624973bbe14ac444aee58325a5019
preethuj <preethu@qburst.com>
2016-07-08 13:21:39 +0530

    Update index.php

--- project.local/public_html/index.php
+++ project.local/public_html/index.php
@@ -3,7 +3,7 @@
 <title>Project</title>
 </head>
 <body>
-<h1>Opinion MIning of Product Reviews</h1>
+<h1>Opinion Mining of Product Reviews</h1>
 <p>Social media is emerging rapidly on the internet. This media knowledge helps people,
 
 company and organizations to analyze information for important decision
